6fbbe4be4415df2b3265bdeb4990de130dbd7524
itpeopleperformance <it@people-performance.com>
2024-06-07 16:31:37 -0300

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('sopinet_chat');
+        $treeBuilder = new TreeBuilder('');
+        $rootNode = $treeBuilder->getRootNode('sopinet_chat');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
